f47cf953f053e944b18c8566355c4fa60109dff9
she4ki <she4ki@gmail.com>
2026-01-06 23:26:50 +0300

    Change video title from '/video' to '/Видео'

--- synology_dlna.js
+++ synology_dlna.js
@@ -20,7 +20,7 @@
           body.append(scroll.render(true));
           tree = {
             device: {name: server},
-            tree: [{title:"/video", id: 1}]
+            tree: [{title:"/Видео", id: 33}]
           };
           this.displayFolder();
         } else {
